78db49216f7c7e1c471ff71fdc70e5ac901d354c
Sivlia <silvolivm@gmail.com>
2023-03-16 20:05:57 +0100

    add archive app

---
+++ src/components/app/app.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { AppRouter } from "../app-router/app-router";
+import { Header } from "../header/header";
+import { Menu } from "../menu/menu";
+import "./app.css";
+
+export type MenuOption = {
+  label: string;
+  path: string;
+  image?: string;
+};
+export const menuOptions: MenuOption[] = [
+  { label: "Home", path: "/home" },
+  { label: "Profile", path: "/profile" },
+];
+
+export function App() {
+  return (
+    <>
+      <Header>
+        <Menu options={menuOptions}></Menu>
+      </Header>
+      <AppRouter menuOptions={menuOptions}></AppRouter>
+    </>
+  );
+}
+
+export default App;
