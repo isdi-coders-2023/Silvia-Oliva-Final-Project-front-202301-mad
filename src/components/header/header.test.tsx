d487fd7a9f3136db93d9b948a9e75ad39459333d
Sivlia <silvolivm@gmail.com>
2023-03-18 22:27:12 +0100

    add header and test

---
+++ src/components/header/header.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import { Header } from "./header";
+
+describe("Given the Header component", () => {
+  describe("when you enter in the component", () => {
+    test("then it should render it", () => {
+      render(
+        <Header>
+          <></>
+        </Header>
+      );
+      const element = screen.getByRole("heading");
+      expect(element).toBeInTheDocument();
+    });
+  });
+});
---
+++ src/components/header/header.tsx
@@ -0,0 +1,10 @@
+type HeaderProps = { children: JSX.Element };
+
+export function Header({ children }: HeaderProps) {
+  return (
+    <header>
+      <h1 className="header__title">Amigurumis</h1>
+      {children}
+    </header>
+  );
+}
