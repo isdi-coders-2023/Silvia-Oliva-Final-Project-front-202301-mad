96b0dd0b4edf9ac74bd322c8fa07a24153054fe4
Sivlia <silvolivm@gmail.com>
2023-03-19 15:32:04 +0100

    add home  archive

--- src/components/home/home.test.tsx
+++ src/components/home/home.test.tsx
@@ -1,5 +1,34 @@
-export function Home() {
-  return <h2>Home</h2>;
-}
+/* eslint-disable testing-library/no-render-in-setup */
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../../store/store";
+import Register from "../register/register";
+import Home from "./home";
 
-export default Home;
+jest.mock("../register/register");
+
+beforeEach(() => {
+  render(
+    <Provider store={store}>
+      <Home></Home>
+    </Provider>
+  );
+});
+
+describe("Given the home component", () => {
+  describe("when we render it", () => {
+    test("then register have ben called", () => {
+      expect(Register).toHaveBeenCalled();
+    });
+  });
+  describe("when we check the buttons", () => {
+    test("then they should be in the document", () => {
+      const button = screen.getAllByRole("button");
+
+      fireEvent.click(button[0]);
+      fireEvent.click(button[1]);
+      expect(button[0]).toBeInTheDocument();
+      expect(button[1]).toBeInTheDocument();
+    });
+  });
+});
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import { LogIn } from "../login/login";
-import { Register } from "../register/register";
+import LogIn from "../login/login";
+import Register from "../register/register";
 
-export function Profile() {
+export function Home() {
   const [isInLogin, setIsInLogin] = useState(false);
 
   const handlerChange = (condition: boolean) => {
@@ -11,11 +11,11 @@ export function Profile() {
 
   return (
     <>
-      <h2>Start</h2>
+      <h2>Home</h2>
       <button onClick={() => handlerChange(false)}>Register</button>
       <button onClick={() => handlerChange(true)}>Login</button>
       {isInLogin ? <LogIn></LogIn> : <Register></Register>}
     </>
   );
 }
-export default Profile;
+export default Home;
